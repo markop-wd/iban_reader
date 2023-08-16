c30d1925cee9cf6db7abe47ffad4abd73a08f52a
MarkoPomorisac <markop@mailbox.org>
2023-08-16 17:14:22 +0200

    methods for static data

---
+++ validator/static_api.py
@@ -0,0 +1,14 @@
+import json
+
+
+def get_countries():
+    with open("static/countries.json") as countries_fp:
+        countries = json.load(countries_fp)
+    return countries
+
+
+def get_banks(country: str):
+    with open("static/banks.json") as banks_fp:
+        banks = json.load(banks_fp)
+    country_banks = banks.get(country, None)
+    return country_banks
