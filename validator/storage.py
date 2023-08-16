a951bc52ed9bafdf563af720a844fda63eb97cc3
MarkoPomorisac <markop@mailbox.org>
2023-08-16 17:14:38 +0200

    storage api

---
+++ validator/storage.py
@@ -0,0 +1,15 @@
+def store_iban(iban: str):
+    with open("storage.json", "a") as storage_fp:
+        storage_fp.write(f"{iban}\n")
+
+
+def display_ibans():
+    iban_list = _return_ibans()
+    print(iban_list)
+    return iban_list
+
+
+def _return_ibans():
+    with open("storage.json", "r") as storage_fp:
+        iban_list = storage_fp.readlines()
+    return iban_list
